8a9ceca3216aa62cbaafdbd9690f0fd07284ce3e
Elliot <elliotandrewwilson@gmail.com>
2023-02-19 20:02:08 -0500

    add deletion prompt

--- find_dupes.py
+++ find_dupes.py
@@ -3,7 +3,7 @@ import sys
 
 directory_path = sys.argv[1]
 
-def get_subdirectories_and_files(path):    
+def get_subdirectories_and_sorted_files(path):    
     scan_results = os.scandir(path)
     subdirectories = []
     files = []
@@ -12,27 +12,56 @@ def get_subdirectories_and_files(path):
             subdirectories.append(scan_result)
         elif scan_result.is_file():
             files.append(scan_result)
-    return files, subdirectories
+    sorted_files = sorted(files, key=lambda file: is_flac(file))
+    return sorted_files, subdirectories
 
-def contains_both_mp3_and_FLAC(file_array):
+def is_mp3(file):
+    return file.name.lower().endswith("mp3")
+
+def is_flac(file):
+    return file.name.lower().endswith("flac")
+
+def contains_both_mp3_and_FLAC(files):
     mp3_count = 0
-    FLAC_count = 0
+    flac_count = 0
 
-    for file in file_array:
+    for file in files:
         if file.name.lower().endswith("mp3"):
             mp3_count += 1
         elif file.name.lower().endswith("flac"):
-            FLAC_count += 1
+            flac_count += 1
         
-        if mp3_count and FLAC_count:
+        if mp3_count and flac_count:
             return True
     
     return False
 
+def find_mp3s(files):
+    mp3s = []
+    for file in files:
+        if file.name.lower().endswith("mp3"):
+            mp3s.append(file)
+    return mp3s
+
+def prompt_delete_dupes(path, files):
+    for file in files:
+        print(file.name)
+    response = input("Delete? y/n: ")
+    if response.lower() == "y":
+        for file in find_mp3s(files):
+            os.remove(file.path)
+        print("Successfully deleted")
+
+    else:
+        pass # replace with writing to log file
+    print("")
+
+
 def recursively_search_subdirectories_for_mixed_music_files(path):
-    files, subdirectories = get_subdirectories_and_files(path)
+    print(path)
+    files, subdirectories = get_subdirectories_and_sorted_files(path)
     if len(files) and contains_both_mp3_and_FLAC(files):
-        print(path)
+        prompt_delete_dupes(path, files)
     if len(subdirectories):
         for subdirectory in subdirectories:
             recursively_search_subdirectories_for_mixed_music_files(subdirectory.path)
